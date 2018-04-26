d9beb77d0e9738d4680cc815a4df30b5528d61f0
Lukas\Desktop1 <lukasvantilburg@gmail.com>
2018-04-25 21:12:51 -0400

    Some testing on waveforms, envelope settings, and added a pot reader for the lowpass filter

--- pianoSynth.ino
+++ pianoSynth.ino
@@ -5,34 +5,30 @@
 #include <SD.h>
 #include <SerialFlash.h>
 
-#include "BinaryDisplay.h"
-#include "PlayManager.h"
-
 // GUItool: begin automatically generated code
-AudioSynthWaveform       waveform2;      //xy=116,453
-AudioSynthWaveform       waveform3;      //xy=117,374
-AudioSynthWaveform       waveform4;      //xy=117,494
-AudioSynthWaveform       waveform1;      //xy=118,335
-AudioEffectMultiply      multiply2;      //xy=261,482
-AudioEffectMultiply      multiply1;      //xy=274,352
-AudioEffectEnvelope      envelope2;      //xy=399,449
-AudioEffectEnvelope      envelope1;      //xy=434,371
-AudioMixer4              mixer1;         //xy=633,387
-AudioFilterStateVariable filter1;        //xy=763,387
-AudioOutputI2S           i2s1;           //xy=899,380
+AudioSynthWaveform       waveform2;      //xy=167,179
+AudioSynthWaveform       waveform4;      //xy=167,245
+AudioSynthWaveform       waveform1;      //xy=169,53
+AudioSynthWaveform       waveform3;      //xy=172,115
+AudioEffectMultiply      multiply1;      //xy=348,83
+AudioEffectMultiply      multiply2;      //xy=351,192
+AudioEffectEnvelope      envelope2;      //xy=507,199
+AudioEffectEnvelope      envelope1;      //xy=511,87
+AudioMixer4              mixer1;         //xy=646,146
+AudioFilterStateVariable filter1;        //xy=800,244
+AudioOutputI2S           i2s1;           //xy=959,238
 AudioConnection          patchCord1(waveform2, 0, multiply2, 0);
-AudioConnection          patchCord2(waveform3, 0, multiply1, 1);
-AudioConnection          patchCord3(waveform4, 0, multiply2, 1);
-AudioConnection          patchCord4(waveform1, 0, multiply1, 0);
-AudioConnection          patchCord5(multiply2, envelope2);
-AudioConnection          patchCord6(multiply1, envelope1);
+AudioConnection          patchCord2(waveform4, 0, multiply2, 1);
+AudioConnection          patchCord3(waveform1, 0, multiply1, 0);
+AudioConnection          patchCord4(waveform3, 0, multiply1, 1);
+AudioConnection          patchCord5(multiply1, envelope1);
+AudioConnection          patchCord6(multiply2, envelope2);
 AudioConnection          patchCord7(envelope2, 0, mixer1, 1);
 AudioConnection          patchCord8(envelope1, 0, mixer1, 0);
 AudioConnection          patchCord9(mixer1, 0, filter1, 0);
-AudioConnection          patchCord10(mixer1, 0, filter1, 1);
-AudioConnection          patchCord11(filter1, 0, i2s1, 0);
-AudioConnection          patchCord12(filter1, 0, i2s1, 1);
-AudioControlSGTL5000     sgtl5000_1;     //xy=234,584
+AudioConnection          patchCord10(filter1, 1, i2s1, 0);
+AudioConnection          patchCord11(filter1, 1, i2s1, 1);
+AudioControlSGTL5000     sgtl5000_1;     //xy=646,28
 // GUItool: end automatically generated code
 
 
@@ -40,28 +36,38 @@ const int note1OnPin = 24;
 const int note2OnPin = 25;
 Bounce *note1OnBounce = new Bounce();
 Bounce *note2OnBounce = new Bounce();
-
+#define potPin A20
 
 void setup() {
-  AudioMemory(18);
+  AudioMemory(30);
   Serial.begin(9600);
-  
+
   waveform1.begin(1.0, 440.0, WAVEFORM_SINE);
   waveform3.begin(1.0, 444.0 / 2, WAVEFORM_SINE);
-  
+
   waveform2.begin(1.0, 493.88, WAVEFORM_SINE);
   waveform4.begin(1.0, 493.88 / 2, WAVEFORM_SINE);
-  
+
   filter1.frequency(400);
   filter1.resonance(0.7);
   filter1.octaveControl(1);
 
-  pinMode(note1OnPin, INPUT);  
+  envelope1.hold(50);
+  envelope2.hold(50);
+  envelope1.decay(100);
+  envelope2.decay(100);
+  envelope1.sustain(0.4);
+  envelope2.sustain(0.4);
+
+  //Turn pot
+  //pinMode(potPin, INPUT);
+
+  pinMode(note1OnPin, INPUT);
   //digitalWrite(inputPin ,HIGH);
   note1OnBounce->attach(note1OnPin);
   note1OnBounce->interval(5);
 
-  pinMode(note2OnPin, INPUT);  
+  pinMode(note2OnPin, INPUT);
   //digitalWrite(inputPin ,HIGH);
   note2OnBounce->attach(note2OnPin);
   note2OnBounce->interval(5);
@@ -69,6 +75,11 @@ void setup() {
 
 
 void loop() {
+  //Filter update with pots
+  int filterPot = analogRead(potPin);
+  filter1.frequency((filterPot / 1024.0) * 500.0);
+  Serial.println(filterPot);
+
   //Turn on note if the button rose and off if it fell
   note1OnBounce->update();
   if (note1OnBounce->rose()) {

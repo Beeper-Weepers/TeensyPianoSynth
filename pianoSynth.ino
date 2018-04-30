b9f152e9329df70531f6dde4754c230d4a9d6f89
Lukas\Laptop2 <lukasvantilburg@gmail.com>
2018-04-30 16:48:32 -0400

    Changes some patches, waveform types, envelope and LFO settings

--- pianoSynth.ino
+++ pianoSynth.ino
@@ -6,20 +6,20 @@
 #include <SerialFlash.h>
 
 // GUItool: begin automatically generated code
-AudioSynthWaveform       waveform4;      //xy=160,419
-AudioSynthWaveform       waveform2;      //xy=161,365
-AudioSynthWaveform       waveform3;      //xy=168,299
-AudioSynthWaveform       waveform1;      //xy=169,250
-AudioEffectMultiply      multiply1;      //xy=370,269
-AudioEffectMultiply      multiply2;      //xy=370,385
-AudioEffectEnvelope      envelope1;      //xy=524,279
-AudioEffectEnvelope      envelope2;      //xy=526,377
-AudioMixer4              mixer1;         //xy=766,251
-AudioSynthWaveformSine   sine1;          //xy=770,334
-AudioEffectMultiply      multiply3;      //xy=959,245
-AudioFilterStateVariable filter1;        //xy=1102,252
-AudioEffectReverb        reverb1;        //xy=1250,250
-AudioOutputI2S           i2s1;           //xy=1421,256
+AudioSynthWaveform       waveform4;      //xy=105,410
+AudioSynthWaveform       waveform2;      //xy=106,356
+AudioSynthWaveform       waveform3;      //xy=113,290
+AudioSynthWaveform       waveform1;      //xy=118,240
+AudioEffectMultiply      multiply1;      //xy=315,260
+AudioEffectMultiply      multiply2;      //xy=315,376
+AudioEffectEnvelope      envelope1;      //xy=469,270
+AudioEffectEnvelope      envelope2;      //xy=471,368
+AudioMixer4              mixer1;         //xy=711,242
+AudioSynthWaveform       waveform5;      //xy=788,310
+AudioEffectMultiply      multiply3;      //xy=904,236
+AudioFilterStateVariable filter1;        //xy=1047,243
+AudioEffectReverb        reverb1;        //xy=1195,241
+AudioOutputI2S           i2s1;           //xy=1366,247
 AudioConnection          patchCord1(waveform4, 0, multiply2, 1);
 AudioConnection          patchCord2(waveform2, 0, multiply2, 0);
 AudioConnection          patchCord3(waveform3, 0, multiply1, 1);
@@ -29,12 +29,13 @@ AudioConnection          patchCord6(multiply2, envelope2);
 AudioConnection          patchCord7(envelope1, 0, mixer1, 0);
 AudioConnection          patchCord8(envelope2, 0, mixer1, 1);
 AudioConnection          patchCord9(mixer1, 0, multiply3, 0);
-AudioConnection          patchCord10(sine1, 0, multiply3, 1);
+AudioConnection          patchCord10(waveform5, 0, multiply3, 1);
 AudioConnection          patchCord11(multiply3, 0, filter1, 0);
 AudioConnection          patchCord12(filter1, 0, reverb1, 0);
 AudioConnection          patchCord13(reverb1, 0, i2s1, 0);
 AudioConnection          patchCord14(reverb1, 0, i2s1, 1);
 // GUItool: end automatically generated code
+
 
 const int note1OnPin = 24;
 const int note2OnPin = 25;
@@ -43,7 +44,7 @@ Bounce *note2OnBounce = new Bounce();
 #define potPin A20
 
 void setup() {
-  AudioMemory(40);
+  AudioMemory(50);
   Serial.begin(9600);
 
   waveform1.begin(1.0, 440.0, WAVEFORM_SINE);
@@ -52,21 +53,22 @@ void setup() {
   waveform2.begin(1.0, 493.88, WAVEFORM_SINE);
   waveform4.begin(1.0, 493.88 / 2, WAVEFORM_SINE);
 
-  filter1.frequency(400);
-  filter1.resonance(0.7);
-  filter1.octaveControl(1);
+  filter1.frequency(500);
+  filter1.resonance(0.707);
+  filter1.octaveControl(4);
 
   envelope1.hold(100);
   envelope2.hold(100);
-  envelope1.decay(800);
-  envelope2.decay(800);
+  envelope1.decay(1500);
+  envelope2.decay(1500);
   envelope1.sustain(0.0); //volume
   envelope2.sustain(0.0);
+  envelope1.release(700);
+  envelope2.release(700);
 
-  sine1.amplitude(1.0);
-  sine1.frequency(50);
+  waveform5.begin(1.0, 500, WAVEFORM_SAWTOOTH);
 
-  reverb1.reverbTime(0.1);
+  reverb1.reverbTime(0.05);
 
   //Turn pot
   //pinMode(potPin, INPUT);
@@ -86,7 +88,7 @@ void setup() {
 void loop() {
   //Filter update with pots
   int filterPot = analogRead(potPin);
-  sine1.frequency((filterPot / 1024.0) * 500.0);
+  waveform5.frequency((filterPot / 1024.0) * 600.0);
 
   //Turn on note if the button rose and off if it fell
   note1OnBounce->update();
